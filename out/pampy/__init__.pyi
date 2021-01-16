d62113aacced5433cdc1190c445f7d2fc8c4f693
kazuki <pharcydetip@gmail.com>
2021-01-16 16:43:46 +0900

    chore: add pampy stub

---
+++ out/pampy/__init__.pyi
@@ -0,0 +1,6 @@
+from pampy.pampy import ANY as ANY, HEAD as HEAD, MatchError as MatchError, REST as REST, TAIL as TAIL, \
+  match as match, match_dict as match_dict, match_iterable as match_iterable, match_value as match_value, _ as _
+from typing import Any
+
+major: Any
+minor: Any
