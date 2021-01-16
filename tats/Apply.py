7dcef3cd508689eb2ef3c74c4b1ecb008c52a35c
kazuki <pharcydetip@gmail.com>
2021-01-16 22:24:02 +0900

    refactor: impl as classmethod

--- tats/Apply.py
+++ tats/Apply.py
@@ -18,16 +18,24 @@ class Apply(Generic[URI], Functor[URI]):
   def ap(ff: Kind1[URI, UnOp[A, B]], fa: Kind1[URI, A]) -> Kind1[URI, B]:
     ...
 
+  @classmethod
+  def product_r(cls, fa: Kind1[URI, A], fb: Kind1[URI, B]) -> Kind1[URI, B]:
+    return cls.ap(cls.map(fa, lambda _: lambda b: b), fb)
+
+  @classmethod
+  def product_l(cls, fa: Kind1[URI, A], fb: Kind1[URI, B]) -> Kind1[URI, A]:
+    return cls.ap(cls.map(fa, lambda a: lambda _: a), fb)
+
 
 def apply_syntax(instance: Type[Apply[URI]]):
 
   def _add_syntax(c):
 
     def _product_r(self, fb):
-      return instance.ap(instance.map(self, lambda _: lambda b: b), fb)
+      return instance.product_r(self, fb)
 
     def _product_l(self, fb):
-      return instance.ap(instance.map(self, lambda a: lambda _: a), fb)
+      return instance.product_l(self, fb)
 
     setattr(c, "product_r", _product_r)
     setattr(c, "product_l", _product_l)
