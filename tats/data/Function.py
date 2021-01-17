6a5a50dadf9949cf02ebf7c9d4bc7cd1980e346e
kazuki <pharcydetip@gmail.com>
2021-01-18 00:51:53 +0900

    refactor: rename Arg to Arg1

--- tats/data/Function.py
+++ tats/data/Function.py
@@ -1,6 +1,6 @@
 from typing import TypeVar, Protocol, Generic
 
-Arg = TypeVar("Arg", contravariant=True)
+Arg1 = TypeVar("Arg1", contravariant=True)
 Res = TypeVar("Res", covariant=True)
 T = TypeVar("T")
 
@@ -8,38 +8,38 @@ A = TypeVar("A")
 B = TypeVar("B")
 
 
-class Func1(Protocol[Arg, Res]):
+class Func1(Protocol[Arg1, Res]):
 
-  def __call__(self, a: Arg) -> Res:
+  def __call__(self, a: Arg1) -> Res:
     ...
 
 
-class Func1F(Generic[Arg, Res]):
+class Func1F(Generic[Arg1, Res]):
 
   @staticmethod
-  def true() -> Func1[Arg, bool]:
+  def true() -> Func1[Arg1, bool]:
     return lambda _: True
 
   @staticmethod
-  def false() -> Func1[Arg, bool]:
+  def false() -> Func1[Arg1, bool]:
     return lambda _: False
 
   @staticmethod
-  def id() -> Func1[Arg, Res]:
+  def id() -> Func1[Arg1, Res]:
     return lambda x: x
 
   @staticmethod
-  def eq(e: Arg) -> Func1[Arg, bool]:
+  def eq(e: Arg1) -> Func1[Arg1, bool]:
     return lambda x: x == e
 
   @staticmethod
-  def const(a: A) -> Func1[Arg, A]:
+  def const(a: A) -> Func1[Arg1, A]:
     return lambda _: a
 
 
-class BiOp(Protocol[Arg, Res]):
+class BiOp(Protocol[Arg1, Res]):
 
-  def __call__(self, a: Arg, b: Arg) -> Res:
+  def __call__(self, a: Arg1, b: Arg1) -> Res:
     ...
 
 
