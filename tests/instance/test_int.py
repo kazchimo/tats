43cc2b41544a3f5d793198b21c8b286f4a15e58c
kazuki <pharcydetip@gmail.com>
2021-01-18 13:50:20 +0900

    test: int instance

---
+++ tests/instance/test_int.py
@@ -0,0 +1,9 @@
+from tats.instance.int_instances import IntInstance
+
+
+class TestIntInstance:
+    def test_combine(self):
+        assert IntInstance().combine(1, 2) == 3
+
+    def test_empty(self):
+        assert IntInstance().empty == 0
