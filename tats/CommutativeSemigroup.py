4f5c1a23521bdfdc983737d6dffd58435661b60f
kazuki <pharcydetip@gmail.com>
2021-01-18 20:11:20 +0900

    feat: add CommutativeSemigroup

---
+++ tats/CommutativeSemigroup.py
@@ -0,0 +1,12 @@
+from abc import ABC
+from typing import TypeVar
+
+from tats.Semigroup import Semigroup
+
+T = TypeVar("T")
+
+
+class CommutativeSemigroup(Semigroup[T], ABC):
+
+  def reverse(self) -> "Semigroup[T]":
+    return self
